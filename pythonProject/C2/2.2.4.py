dec6b62d7ffc08ad20e2debefe77388d645359d9
Sergey <yorke@inbox.ru>
2022-06-01 14:00:26 +0700

    add warship game

--- pythonProject/C2/2.2.4.py
+++ pythonProject/C2/2.2.4.py
@@ -7,7 +7,9 @@ class SquareFactory:
     def create_square(side):
         return Square(side)
 
+
 sq1 = SquareFactory.create_square(2)
 print(sq1.side)
 sq1.side = 3
 print(sq1.side)
+print(isinstance(sq1, Square))
